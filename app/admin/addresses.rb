38f008d256800e49261876823439aa25d8741b1b
Dinesh Panda <dineshpanda92@gmail.com>
2020-05-20 16:13:25 +0530

    Whitelist columns in Address for ActiveAdmin

--- app/admin/addresses.rb
+++ app/admin/addresses.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Address do
 
+ permit_params :employee_id, :office_addr
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
