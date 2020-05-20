9a91cc573f9803989d5c1d93998fabd2b64b3ed1
Dinesh Panda <dineshpanda92@gmail.com>
2020-05-20 16:14:19 +0530

    Install Kaminari

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,6 +1,6 @@
 class AddressesController < ApplicationController
   def index
-    @addresses = Address.all
+    @addresses = Address.page(params[:page]).per(10)
     @location_hash = Gmaps4rails.build_markers(@addresses.where.not(:office_addr_latitude => nil)) do |address, marker|
       marker.lat address.office_addr_latitude
       marker.lng address.office_addr_longitude
